5d1e38d87ff9da0aa333fc7b49251810a99e041d
Suriyasak-Integrity <193051942+Suriyasak-Integrity@users.noreply.github.com>
2025-03-23 18:41:50 +0700

    Add Webpack configuration to support ES Modules

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+module.exports = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          sourceType: 'module',  // เพิ่มการตั้งค่า sourceType เป็น module
+        },
       },
-    ],
-  },
-  typescript: {
-    // Ignore TypeScript errors during build to allow deployment
-    ignoreBuildErrors: true,
+    });
+    return config;
   },
 };
-
-module.exports = nextConfig;
