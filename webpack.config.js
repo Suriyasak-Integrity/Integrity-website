50e0952ad901dfcbe4968c7cdfa74fc101f827a8
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2025-03-24 16:01:41 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.js$/,             // ✅ ใช้กับไฟล์ .js
+        exclude: /node_modules/,   // ✅ ไม่รวม node_modules
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            sourceType: 'module',  // เพิ่มการตั้งค่า sourceType เป็น module
+            presets: ['@babel/preset-env', '@babel/preset-react'], 
+            sourceType: 'module'   // ✅ จุดสำคัญ!! ให้ Babel อ่านเป็น ES Module
           },
         },
       },
