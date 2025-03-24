29ab76237dd61f0185e8e58246e7265535ae3bf6
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2025-03-25 03:25:40 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
-const inter = Inter({
-  subsets: ['latin'],
+const inter = Inter({subsets: ['latin'],
 });
 
 export const metadata = {
