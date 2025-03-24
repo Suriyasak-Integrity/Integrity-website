d253fe0a79167daa388f108c82cd445a6a39ddc4
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2025-03-24 15:09:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.js$/,
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          sourceType: 'module',
+          plugins: ['@babel/plugin-transform-runtime'],
+          sourceType: 'unambiguous' // Add this line
         },
       },
     });
