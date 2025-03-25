6b386c396fc43703e776d8e6f42777ccc7a140a5
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2025-03-25 08:40:48 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
-
-const inter = Inter({subsets: ['latin'],
-});
-
+const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Integrity Logistics Thailand - Beyond the Average Freight Forwarder',
   description: 'Professional freight forwarding and logistics solutions.',
